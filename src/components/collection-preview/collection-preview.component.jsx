74b7d54fec9a4fbeda14e07391c530f0dbf7ceb8
John Wells <molekewl2017@gmail.com>
2019-09-24 11:14:51 -0700

    connected collection Item to collection preview

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CollectionItem from '../collection-item/collection-item.component';
+
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => (
@@ -9,8 +11,8 @@ const CollectionPreview = ({ title, items }) => (
       {
         items
           .filter((item, i) => i < 4)
-          .map(item => (
-          <div key={item.id}>{item.name}</div>
+          .map(({ id, ...otherItemProps}) => (
+          <CollectionItem key={id} {...otherItemProps} />
         ))
       }
     </div>
