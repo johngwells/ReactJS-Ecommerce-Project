d960d52db351066bcb90c6be0c85f51ce69e78ed
John Wells <molekewl2017@gmail.com>
2019-09-29 07:49:26 -0700

    top nav link changes if signed in or out

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
 
     this.state = {
       currentUser: null
-    }
+    };
 
   }
 
@@ -23,9 +23,9 @@ class App extends React.Component {
 
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+      this.setState({ currentUser: user });
       console.log(user);
-    })
+    });
   }
 
   componentWillUnmount() {
@@ -35,7 +35,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
