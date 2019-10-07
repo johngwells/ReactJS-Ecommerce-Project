950be9e30064a9b1d29daff633d4287513be2303
John Wells <molekewl2017@gmail.com>
2019-10-06 23:58:07 -0700

    edit app.js change setState to setCurrentUser with redux

--- src/App.js
+++ src/App.js
@@ -15,24 +15,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
-          }, () => {
-            console.log(this.state);
           });
         });
-      } 
-      else {
-        this.setState({ currentUser: userAuth });
       }
+      
+      setCurrentUser(userAuth);
     });
   }
 
