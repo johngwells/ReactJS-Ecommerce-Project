4999e2e677ee52e907edbd0a5b437a682b3183a1
John Wells <molekewl2017@gmail.com>
2019-10-07 00:32:35 -0700

    add userActionTypes

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,3 +1,5 @@
+import { UserActionTypes } from './user.types';
+
 // initial state is the default state
 const INITIAL_STATE = {
   currentUser: null
@@ -6,7 +8,7 @@ const INITIAL_STATE = {
 // if states is undefined or not set
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'SET_CURRENT_USER':
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload
