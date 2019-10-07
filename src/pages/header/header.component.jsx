03b6fcd93411d5b0e85055c187b73680d77306d3
John Wells <molekewl2017@gmail.com>
2019-10-07 07:56:10 -0700

    dropdown toggles

--- src/pages/header/header.component.jsx
+++ src/pages/header/header.component.jsx
@@ -10,7 +10,7 @@ import { connect } from 'react-redux';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo'/>
@@ -34,12 +34,16 @@ const Header = ({ currentUser }) => (
       }
       <CartIcon />
     </div>
-    <CartDropdown />
+    {
+      hidden ? null :
+      <CartDropdown />
+    }
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
