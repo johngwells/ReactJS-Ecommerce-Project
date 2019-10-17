a9a5b3d8353eb7f39024f8079e6fdf31687076d7
John Wells <molekewl2017@gmail.com>
2019-10-17 00:57:15 -0700

    connect redux to components

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -11,8 +11,8 @@ const CollectionPreview = ({ title, items }) => (
       {
         items
           .filter((item, i) => i < 4)
-          .map(({ id, ...otherItemProps}) => (
-          <CollectionItem key={id} {...otherItemProps} />
+          .map(item => (
+          <CollectionItem key={item.id} item={item} />
         ))
       }
     </div>
