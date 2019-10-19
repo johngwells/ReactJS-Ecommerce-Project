e3d2ea62f90356c6fe72a1569a6fa9ef373b35ab
John Wells <molekewl2017@gmail.com>
2019-10-19 01:50:56 -0700

    add or remove item from checkout pge

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -9,7 +9,26 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
         ? {...cartItem, quantity: cartItem.quantity + 1}
         : cartItem
       );
-  };
+  }
 
   return [...cartItems, {...cartItemToAdd, quantity: 1}];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  )
+
+  // if 1 remove it
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+  }
+
+  // decrease quantity
+  return cartItems.map(
+    cartItem =>
+    cartItem.id === cartItemToRemove.id 
+      ? {...cartItem, quantity: cartItem.quantity - 1}
+      : cartItem
+  );
 };
