6be7509d9e129c84d28819dfe86f89468c70a4a4
John Wells <molekewl2017@gmail.com>
2019-10-20 16:18:08 -0700

    connect redux persist to root reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,8 +6,8 @@ import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export const persistor = persistStore(store);
+const persistor = persistStore(store);
 
 export default { store, persistor };
