9e3fcc024ff2a302ffbbf03aa92fd16f0622ac33
John Wells <molekewl2017@gmail.com>
2019-10-20 23:04:02 -0700

    create stripe button logic

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  // stripe wants dollars into cents
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_3qPooZElqsgrXql7HJCyWWei00eUKhrhEe';
+
+
+  // doing it this way for now. Used to charge payments on backend. 
+  // Once backend is set. configure this.
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  }
+
+  return (
+    <StripeCheckout 
+      label='Pay Now'
+      name='Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='http://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+}
+
+export default StripeCheckoutButton;
