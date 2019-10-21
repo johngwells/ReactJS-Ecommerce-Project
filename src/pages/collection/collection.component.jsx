cfd557b5570a48aa891078db8ba3a5950cd4ca02
John Wells <molekewl2017@gmail.com>
2019-10-20 18:33:30 -0700

    create collection page & styles

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => (
+  <div className='collection-page'>
+    <h2>COLLECTION PAGE</h2>
+  </div>
+);
+
+export default CollectionPage;
